421233757f28483f903757528e06b950878ce1fd
baltzelj <baltzelj@gmail.com>
2024-07-16 09:31:59 -0700

    Cleaned some of the columns, working on changing datatypes.

--- dogbites.py
+++ dogbites.py
@@ -1,10 +1,13 @@
+# Importing dependencies that will be used for the code.
 import json
 import pandas as pd
 from pprint import pprint
+import datetime
 
+# Importing json data.
 json_data = json.load(open("dogbites.json"))
 
-# creating a data specific DataFrame.
+# Creating a data specific DataFrame.
 dog_data = pd.DataFrame(json_data["data"], columns=[
   "drop_code1",
   "drop_code2",
@@ -14,18 +17,18 @@ dog_data = pd.DataFrame(json_data["data"], columns=[
   "drop_code6",
   "drop_code7",
   "drop_code8",
-  "UniqueID",
+  "unique_id",
   "incident_date",
   "species",
   "breed",
   "age",
   "gender",
-  "spay/neuter_status",
+  "spay_neuter_status",
   "borough",
   "zip_code"
 ])
 
-# specifying columns to drop.
+# Specifying columns to drop.
 drop_columns = [
   "drop_code1",
   "drop_code2",
@@ -37,7 +40,14 @@ drop_columns = [
   "drop_code8"
 ]
 
+# Cleaning column headers.
 dog_data_clean = dog_data.drop(columns=drop_columns)
-dog_data_clean.sort_values("UniqueID")
-print(dog_data_clean)
+# Notes on cleaning the data:
+# - Inconsistent age formatting was used. Sometimes in months, mostly in years.
+# - Zipcodes labeled in different ways. "?", "N/A", etc.
 
+dog_data_clean["unique_id"] = dog_data_clean["unique_id"].astype("Int64")
+
+# Sorting and printing data.
+# dog_data_clean.sort_values("unique_id")
+print(dog_data_clean.dtypes)
